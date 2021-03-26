00d2e03e76c4bed0290a3744a7e4cbb0eef62db1
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-26 18:09:49 +0100

    finish

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,23 +3,22 @@ const loaded = document.querySelector(".header__button");
 export function getCharacters() {
   let url = "https://rickandmortyapi.com/api/character";
   const filterDropdown = document.querySelector(".dropdown-menu");
-
+  const searchbar = document.querySelector(".searchbar");
+  const characterName = searchbar.value;
   const type = filterDropdown.value;
 
   if (type === "all") {
-    url = "https://rickandmortyapi.com/api/character";
+    url = `https://rickandmortyapi.com/api/character?name=${characterName}`;
   } else if (type === "alive") {
-    url = "https://rickandmortyapi.com/api/character?status=alive";
+    url = `https://rickandmortyapi.com/api/character?name=${characterName}&status=alive`;
   } else if (type === "dead") {
-    url = "https://rickandmortyapi.com/api/character?status=dead";
+    url = `https://rickandmortyapi.com/api/character?name=${characterName}&status=dead`;
   } else if (type === "unknown") {
-    url = "https://rickandmortyapi.com/api/character?status=unknown";
+    url = `https://rickandmortyapi.com/api/character?name=${characterName}&status=unknown`;
   }
 
   fetch(url)
     .then((response) => {
-      loaded.textContent = "Loaded!";
-
       function sleep(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
       }
@@ -34,7 +33,8 @@ export function getCharacters() {
         delayedGreeting();
         return response.json();
       } else {
-        loaded.textContent = "Ooops!";
+        console.log(`Error: ${response.status}`);
+        loaded.textContent = "Select All and Press Here";
       }
     })
     .then((dataObject) => {
@@ -44,6 +44,7 @@ export function getCharacters() {
         section.classList.add("character");
         const main = document.querySelector("main");
         main.classList.add("main");
+
         const h2 = document.createElement("h2");
         h2.classList.add("character-name");
         h2.textContent = character.name;
@@ -51,17 +52,29 @@ export function getCharacters() {
         img.classList.add("character-image");
         img.src = character.image;
         img.alt = character.name;
-        const status = document.createElement("p");
-        status.classList.add("character-status");
-        status.textContent = `Status: ${character.status}`;
+
         const wrapper = document.createElement("div");
         wrapper.classList.add("info-wrapper");
 
+        const status = document.createElement("p");
+        status.classList.add("character-info");
+        status.textContent = `Status: ${character.status}`;
+
+        const species = document.createElement("p");
+        species.classList.add("character-info");
+        species.textContent = `Species: ${character.species}`;
+
+        const gender = document.createElement("p");
+        gender.classList.add("character-info");
+        gender.textContent = `Gender: ${character.gender}`;
+
         main.append(section);
-        section.append(wrapper);
         wrapper.append(h2);
-        wrapper.append(status);
         section.append(img);
+        section.append(wrapper);
+        wrapper.append(status);
+        wrapper.append(species);
+        wrapper.append(gender);
 
         if (character.status === "Alive") {
           section.style.backgroundColor = " #33FF99";
